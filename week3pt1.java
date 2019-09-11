4fcbb5734a66a1b6ef6590406814569e3317d833
natabratz <54905666+natabratz@users.noreply.github.com>
2019-09-11 12:37:01 -0400

    Add files via upload

---
+++ week3pt1.java
@@ -0,0 +1,32 @@
+
+public class week3pt1 {
+	public static void main(String[] args) {
+		
+		int years = 4;
+		int children = 10;
+		double csupport = 25600.75;
+		
+		String man = "Joseph";
+		String woman = "Joana";
+		
+		System.out.printf("%s has %d children with %s but they are no longer together. ", "Joseph", children, "Joana");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
