fceb57eca444ca86a2bc1a9723dc12dfb9cc14ba
natabratz <54905666+natabratz@users.noreply.github.com>
2019-10-16 12:24:43 -0400

    Add files via upload

---
+++ Switch2.java
@@ -0,0 +1,12 @@
+public class Switch2 {
+	public static void main(String[]args) {
+		System.out.println("Here are the answers for Question 5: ");
+		
+		System.out.println("a: \"One\"");
+		System.out.println("b: \"Three\"");
+		System.out.println("c: \"Four\"");
+		System.out.println("d: \"Five\"");
+		System.out.println("e: \"Invalid Number\"");
+		
+	}
+}
